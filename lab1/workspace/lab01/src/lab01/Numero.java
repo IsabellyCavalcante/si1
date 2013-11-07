b37e50c01d21c8d8acf8d52e0b64a5ba27bb04c0
Isabelly <isabelly.cavalcante@ccc.ufcg.edu.br>
2013-11-06 21:43:53 -0300

    versao 1.3

--- lab1/workspace/lab01/src/lab01/Numero.java
+++ lab1/workspace/lab01/src/lab01/Numero.java
@@ -1,14 +1,26 @@
 package lab01;
 
-public class Numero {
+import java.util.Hashtable;
 
-	public Object porExtenso(String num) {
-		if (num.equals("0")) {
-			return "zero";
-		} else if (num.equals("1")) {
-			return "um";
-		} else {
-			return "dois";
-		}
+public class Numero {
+	
+	private Hashtable<String, String> numeros = new Hashtable <String, String>();
+
+	public String porExtenso(String i) {
+		numeros.put("0", "zero");
+		numeros.put("1", "um");
+		numeros.put("2", "dois");
+		numeros.put("3", "tres");
+		numeros.put("4", "quatro");
+		numeros.put("5", "cinco");
+		numeros.put("6", "seis");
+		numeros.put("7", "sete");
+		numeros.put("8", "oito");
+		numeros.put("9", "nove");
+		numeros.put("10", "dez");
+		
+		return numeros.get(i);
+		
 	}
+
 }
