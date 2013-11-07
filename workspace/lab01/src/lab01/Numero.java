afb2d68df67fc0bcc61d6dd379a520503328d4c9
Isabelly <isabelly.cavalcante@ccc.ufcg.edu.br>
2013-11-06 21:21:24 -0300

    versao 1.0

---
+++ workspace/lab01/src/lab01/Numero.java
@@ -0,0 +1,9 @@
+package lab01;
+
+public class Numero {
+
+	public Object porExtenso(String num) {
+		return "zero";
+	}
+
+}
---
+++ workspace/lab01/src/lab01/Teste.java
@@ -0,0 +1,16 @@
+package lab01;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Teste {
+
+	@Test
+	public void test() {
+		Numero num = new Numero();
+		assertEquals("zero", num.porExtenso("0"));		
+		
+	}
+
+}
