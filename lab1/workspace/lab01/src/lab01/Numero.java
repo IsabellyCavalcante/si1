bb2733741494682d25bed177c6c946ed8ec5ee9a
Isabelly <isabelly.cavalcante@ccc.ufcg.edu.br>
2013-11-08 00:54:13 -0300

    versao 1.1

--- lab1/workspace/lab01/src/lab01/Numero.java
+++ lab1/workspace/lab01/src/lab01/Numero.java
@@ -3,7 +3,9 @@ package lab01;
 import java.util.Hashtable;
 
 public class Numero {
-
+	private String centena = "";
+	private String dezena = "";
+	private String unidade = "";
 	private Hashtable<String, String> numeros = new Hashtable<String, String>();
 	private String[] alf = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
 			"k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x",
@@ -11,9 +13,11 @@ public class Numero {
 	private String[] numExtenso = { "zero", "um", "dois", "tres", "quatro",
 			"cinco", "seis", "sete", "oito", "nove", "dez", "onze", "doze",
 			"treze", "quatorze", "quinze", "dezesseis", "dezessete", "dezoito",
-			"dezenove", "vinte"}; 
-	//private String[] dezenas = {"trinta", "quarenta", "cinquenta", "sessenta",
-	//"setenta", "oitenta", "noventa" };
+			"dezenove", "vinte" };
+
+	// private String[] dezenas = {"trinta", "quarenta", "cinquenta",
+	// "sessenta",
+	// "setenta", "oitenta", "noventa" };
 
 	public String porExtenso(String num) throws Exception {
 		// exceçoes
@@ -26,12 +30,25 @@ public class Numero {
 				throw new Exception("Número inválido");
 			}
 		}
-
-		String dezena = num.substring(0, 1);
-		String unidade = num.substring(1);
+		boolean condicao = false;
+		
+		if(num.length() == 2){
+			condicao = true;
+			dezena = num.substring(0, 1);
+			unidade = num.substring(1);
+		}
+		if (num.length() == 3){
+			centena = num.substring(0, 1);
+			dezena = num.substring(1, 2);
+			unidade = num.substring(2);
+		}
+		
 
 		// adiciona no hash
 		for (int i = 0; i < 21; i++) {
+			if(i < 10){
+				numeros.put("0" + String.valueOf(i), numExtenso[i]);
+			}
 			numeros.put(String.valueOf(i), numExtenso[i]);
 		}
 		// adiciona as dezenas
@@ -42,21 +59,58 @@ public class Numero {
 		numeros.put("70", "setenta");
 		numeros.put("80", "oitenta");
 		numeros.put("90", "noventa");
+		
+		//centenas
+		numeros.put("100", "cem");
+		numeros.put("200", "duzentos");
+		numeros.put("300", "trezentos");
+		numeros.put("400", "quatrocentos");
+		numeros.put("500", "quinhentos");
+		numeros.put("600", "seiscentos");
+		numeros.put("700", "setecentos");
+		numeros.put("800", "oitocentos");
+		numeros.put("900", "novecentos");
 
 		if (numeros.containsKey(num)) {
 			return numeros.get(num);
-		} else {
+		} else if (condicao == true) {
 			return dezena(dezena, unidade);
+		} else if (condicao == false){
+			return centena(centena, dezena, unidade);
 		}
+		return num;
 
+	}
+
+	private String centena(String cent, String dez, String unid) {
+		if (cent.equals("1")) {
+			if (dez.equals("0") || dez.equals("1")) {
+				return "cento e " + numeros.get(dez + unid);
+			}
+			return "cento e " + dezena(dez, unid);
+		}
+		
+		if (dez.equals("0") || dez.equals("1")) {
+			return numeros.get(cent + "00") + " e " + numeros.get(dez + unid);
+		}
+		return numeros.get(cent + "00") + " e "+ dezena(dez, unid);
 	}
 
 	private String dezena(String dezena, String unidade) {
+		if (numeros.containsKey(dezena + unidade)){
+			return numeros.get(dezena + unidade);
+		}
 		return numeros.get(dezena + "0") + " e " + numeros.get(unidade);
-		
 	}
 
 	public static void main(String[] args) {
+		Numero num = new Numero();
+		try {
+			System.out.println(num.porExtenso("01"));
+			System.out.println(num.porExtenso("1"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
--- lab1/workspace/lab01/src/lab01/Teste.java
+++ lab1/workspace/lab01/src/lab01/Teste.java
@@ -71,28 +71,110 @@ public class Teste {
 
 		for (int i = 0; i < 9; i++) {
 			try {
-				System.out.println(num1.porExtenso(String.valueOf(i + 21)));
-				assertEquals(numExtenso[i], num1.porExtenso(String.valueOf(i + 21)));
+				assertEquals(numExtenso[i],
+						num1.porExtenso(String.valueOf(i + 21)));
 			} catch (Exception e) {
 				fail("laçou exceçao, mas nao devia");
 			}
 		}
 	}
-	
+
 	@Test
 	public void test30() {
-		String[] numExtenso = { "trinta", "trinta e um", "trinta e dois", "trinta e tres",
-				"trinta e quatro", "trinta e cinco", "trinta e seis",
-				"trinta e sete", "trinta e oito", "trinta e nove", };
+		String[] numExtenso = { "trinta", "trinta e um", "trinta e dois",
+				"trinta e tres", "trinta e quatro", "trinta e cinco",
+				"trinta e seis", "trinta e sete", "trinta e oito",
+				"trinta e nove", };
 
 		for (int i = 0; i < 10; i++) {
 			try {
-				System.out.println(num1.porExtenso(String.valueOf(i + 30)));
-				assertEquals(numExtenso[i], num1.porExtenso(String.valueOf(i + 30)));
+				assertEquals(numExtenso[i],
+						num1.porExtenso(String.valueOf(i + 30)));
 			} catch (Exception e) {
 				fail("laçou exceçao, mas nao devia");
 			}
 		}
 	}
 
+	@Test
+	public void test90() {
+		String[] numExtenso = { "noventa", "noventa e um", "noventa e dois",
+				"noventa e tres", "noventa e quatro", "noventa e cinco",
+				"noventa e seis", "noventa e sete", "noventa e oito",
+				"noventa e nove", };
+
+		for (int i = 0; i < 10; i++) {
+			try {
+				assertEquals(numExtenso[i],
+						num1.porExtenso(String.valueOf(i + 90)));
+			} catch (Exception e) {
+				fail("laçou exceçao, mas nao devia");
+			}
+		}
+	}
+
+	@Test
+	public void test100() {
+		String[] numExtenso = { "cem", "cento e um", "cento e dois",
+				"cento e tres", "cento e quatro", "cento e cinco",
+				"cento e seis", "cento e sete", "cento e oito", "cento e nove", };
+
+		for (int i = 0; i < 10; i++) {
+			try {
+				assertEquals(numExtenso[i],
+						num1.porExtenso(String.valueOf(i + 100)));
+			} catch (Exception e) {
+				fail("laçou exceçao, mas nao devia");
+			}
+		}
+	}
+
+	@Test
+	public void test200() {
+		String[] numExtenso = { "duzentos", "duzentos e um", "duzentos e dois",
+				"duzentos e tres", "duzentos e quatro", "duzentos e cinco",
+				"duzentos e seis", "duzentos e sete", "duzentos e oito",
+				"duzentos e nove", };
+
+		for (int i = 0; i < 10; i++) {
+			try {
+				assertEquals(numExtenso[i],
+						num1.porExtenso(String.valueOf(i + 200)));
+			} catch (Exception e) {
+				fail("laçou exceçao, mas nao devia");
+			}
+		}
+	}
+
+	@Test
+	public void testAleatorioAte999() {
+		try {
+			assertEquals("trezentos e noventa", num1.porExtenso("390"));
+		} catch (Exception e) {
+			fail("laçou exceçao, mas nao devia");
+		}
+		try {
+			assertEquals("cinco", num1.porExtenso("05"));
+		} catch (Exception e) {
+			fail("laçou exceçao, mas nao devia");
+		}
+		
+		try {
+			assertEquals("setecentos e trinta e dois", num1.porExtenso("732"));
+		} catch (Exception e) {
+			fail("laçou exceçao, mas nao devia");
+		}
+		
+		try {
+			assertEquals("setecentos e dois", num1.porExtenso("702"));
+		} catch (Exception e) {
+			fail("laçou exceçao, mas nao devia");
+		}
+
+		try {
+			assertEquals("cento e um", num1.porExtenso("101"));
+		} catch (Exception e) {
+			fail("laçou exceçao, mas nao devia");
+		}
+	}
 }
