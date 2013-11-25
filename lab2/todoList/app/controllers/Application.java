59cfb9761aaeb360246bb27a2c61ff23e6e603c0
Isabelly <isabelly.cavalcante@ccc.ufcg.edu.br>
2013-11-24 22:10:23 -0300

    aplicacao com form

--- lab2/todoList/app/controllers/Application.java
+++ lab2/todoList/app/controllers/Application.java
@@ -1,19 +1,22 @@
 package controllers;
 
 import play.*;
+import play.data.*;
 import play.mvc.*;
-
+import models.*;
 import views.html.*;
 
 public class Application extends Controller {
 
+	static Form<Task> taskForm = Form.form(Task.class);
+	
 	//redirecionando o index para o metodo tasks()
     public static Result index() {
-    	return redirect(routes.Application.newTask());
+    	return redirect(routes.Application.tasks());
     }
     
     public static Result tasks(){
-		return TODO;
+    	return ok(views.html.index.render(Task.all(), taskForm));
     	
     }
     
